ec27cea478f1951d752e8fe107930b8d5ebbfd6b
Alex Figliolia <alexfigliolia@gmail.com>
2023-05-08 12:58:47 -0700

    Remove required 'type' key

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,6 @@
-export type Message<T extends Record<string, any> = Record<string, any>> = {
-  type: string;
-} & T;
+export type MessageMap<T extends Record<string, any> = Record<string, any>> =
+  Record<string, T>;
 
-export type MessageMap = Record<string, Message>;
-
-export type Listener<T extends Message> = (event: T) => void | Promise<void>;
+export type Listener<T extends Record<string, any>> = (
+  event: T
+) => void | Promise<void>;

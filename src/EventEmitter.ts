5260832a0f724959ce2c4e97ea40b43a90e05c20
Alex Figliolia <alexfigliolia@gmail.com>
2023-05-09 10:36:03 -0700

    Rollback extracts

--- src/EventEmitter.ts
+++ src/EventEmitter.ts
@@ -5,24 +5,21 @@ export class EventEmitter<T extends MessageMap> extends Map<
   keyof T,
   Indexer<Listener<any>>
 > {
-  public on<E extends Extract<keyof T, string>>(
-    event: E,
-    listener: Listener<T[E]>
-  ) {
+  public on<E extends keyof T>(event: E, listener: Listener<T[E]>) {
     const index = this.get(event) || new Indexer();
     const ID = index.register(listener);
     this.set(event, index);
     return ID;
   }
 
-  public off<E extends Extract<keyof T, string>>(event: E, ID: string) {
+  public off<E extends keyof T>(event: E, ID: string) {
     const index = this.get(event);
     if (index) {
       return index.delete(ID);
     }
   }
 
-  public emit<E extends Extract<keyof T, string>>(event: E, param: T[E]) {
+  public emit<E extends keyof T>(event: E, param: T[E]) {
     const index = this.get(event);
     if (index) {
       index.execute(param);

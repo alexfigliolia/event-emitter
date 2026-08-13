6c94c9974262fe2309a8848d13e50f6d45431d8b
Alex Figliolia <alexfigliolia@gmail.com>
2026-08-13 14:42:35 -0700

    Spread event payload params, move to pnpm

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
-export type MessageMap<T = any> = Record<string, T>;
+export type MessageMap<T = any[]> = Record<string, T>;
 
 export type Callback<A extends any[] = never[], V = void | Promise<void>> = (
   ...args: A
 ) => V;
 
-export type Listener<T> = Callback<[event: T]>;
+export type Listener<T extends any[]> = Callback<T>;
